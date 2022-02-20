df4cd7c4b454aef35ea9aff6bc7807a41a53ed0e
lesslucifer <less.lucifer@gmail.com>
2022-02-20 18:45:48 +0700

    + add test form

--- routes/forms.ts
+++ routes/forms.ts
@@ -1,12 +1,25 @@
-import { Body, ExpressRouter, POST } from "express-router-ts";
+import { Body, ExpressRouter, GET, POST, ResponseHandler } from "express-router-ts";
 import * as _ from "lodash";
 import moment from "moment";
 import CONN from "../glob/conn";
 import { ValidBody } from "../utils/decors";
+import * as fs from 'fs-extra'
+import * as path from 'path'
 
 class FormRouter extends ExpressRouter {
     document = {
         'tags': ['Ultilities']
+    }
+    
+    @ResponseHandler((data, req, resp) => {
+        resp.writeHead(200, {"Content-Type": "text/html"})
+        resp.write(data)
+        resp.end()
+    })
+    @GET({path: "/test"})
+    async testForm() {
+        const data = await fs.readFile(path.resolve(process.cwd(), 'forms/test.html'))
+        return data
     }
     
     @ValidBody({
